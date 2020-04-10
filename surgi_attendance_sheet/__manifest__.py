3c4de866f1f4e464bd5da59914ed12cf616b241e
ramadan khalil <rkhalil1990@gmail.com>
2020-04-10 15:04:10 +0200

    [ADD]surgi_attendance_sheet:adding new field in contract with name multi shifts [ADD] surgi_attendance_sheet:add cron job to update attendance sheet [ADD] surgi_attendance_sheet:calculate the working hours based on the plaing shifts [ADD] surgi_attendance_sheet:compute attendance sheet up on the current date only

---
+++ surgi_attendance_sheet/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Surgi-Tech Attendance Sheet Customizations",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "Ramadan Khalil",
+    'website': "https://www.linkedin.com/in/ramadan-khalil-a7088164/",
+    'version': '0.1',
+    'depends': ['base','rm_hr_attendance_sheet','planning'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/hr_contract_view.xml'
+    ],
+}
