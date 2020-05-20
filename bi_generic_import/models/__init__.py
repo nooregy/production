eaf0631d0933aeb8d42f524fe5e8e8829b5ce8b1
surgi-tech <59487271+surgi-tech@users.noreply.github.com>
2020-05-20 09:17:53 +0200

    bi_generic_import

--- bi_generic_import/models/__init__.py
+++ bi_generic_import/models/__init__.py
@@ -2,7 +2,7 @@
 # Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
 
 from . import sale
-from . import account_invoice
+#from . import account_invoice
 from . import purchase
 from . import stock
 from . import product
