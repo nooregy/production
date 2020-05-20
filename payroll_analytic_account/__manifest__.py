bb3c01eec07d95a371d91bb6dfa1843e628b2814
surgi-tech <59487271+surgi-tech@users.noreply.github.com>
2020-05-20 08:47:18 +0200

    payroll_analytic_account

--- payroll_analytic_account/__manifest__.py
+++ payroll_analytic_account/__manifest__.py
@@ -34,7 +34,7 @@ Payroll Analytic Account.
     'author': 'Konos',
     'website': 'http://konos.cl',
     'depends': [
-        'hr_payroll_account'
+        'hr_payroll_account',
     ],
     'data': [
         'views/hr_payroll_analytic_account_view.xml',
