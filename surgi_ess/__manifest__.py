df8bda0a7d44938881ae5a56ecf8cf80c310a859
surgi-tech <59487271+surgi-tech@users.noreply.github.com>
2020-06-16 12:05:21 +0200

    surgi_ess

--- surgi_ess/__manifest__.py
+++ surgi_ess/__manifest__.py
@@ -24,12 +24,11 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'security/ir.model.access.csv',
+        'views/employee_directory.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+
     ],
 }
