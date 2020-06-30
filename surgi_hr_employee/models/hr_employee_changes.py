dffb3bf43012ee2ed42f2ecc2c324a38252718d1
surgi-tech <59487271+surgi-tech@users.noreply.github.com>
2020-06-30 22:29:15 +0200

    surgi_hr_employee

--- surgi_hr_employee/models/hr_employee_changes.py
+++ surgi_hr_employee/models/hr_employee_changes.py
@@ -1,26 +1,29 @@
-from odoo import api,_
+from odoo import api, _
 from odoo import fields
 from odoo import models
 from odoo.exceptions import ValidationError
-from datetime import datetime,date
+from datetime import datetime, date
 from dateutil import relativedelta
 
 
-class department_fields(models.Model):
+class DepartmentFields(models.Model):
     _inherit = 'hr.department'
 
     department_type = fields.Selection([('department', 'Main Department'), ('section', 'Section')], string='Type',
                                        translate=True)
-class employee_fields(models.Model):
-    _inherit = 'hr.employee.public'
 
-    labor_linc_no = fields.Char(string="Labor Linc No.")
-    id_from = fields.Char(string="ID From")
+class HrEmployeeBaseDate(models.AbstractModel):
+    _inherit = "hr.employee.base"
+
+    labor_linc_no = fields.Char(string="Labor Linc No.", )
+    id_from = fields.Char(string="ID From", store=True)
     military_status = fields.Selection(string="Military Status",
                                        selection=[('not applicable', 'Not Applicable'), ('postponed', 'Postponed'),
                                                   ('exempted', 'Exempted'), ('completed', 'Completed'),
-                                                  ('current', 'Currently serving ')])
-    military_number = fields.Char(string="Military Number")
+                                                  ('current', 'Currently serving ')], store=True)
+    military_number = fields.Char(string="Military Number", store=True)
+
+
     religion = fields.Selection(string="Religion",
                                 selection=[('muslim', 'Muslim'), ('christian', 'Christian'), ('other', 'Others')])
     social_ins_exist = fields.Boolean("Has Social Insurance")
@@ -51,48 +54,6 @@ class employee_fields(models.Model):
     employee_arabic_name = fields.Char(string="Arabic Name")
     private_num = fields.Char(string="Private Number ", required=False, )
 
-class employee_fields(models.Model):
-    _inherit = 'hr.employee'
-
-    labor_linc_no = fields.Char(string="Labor Linc No.")
-    id_from = fields.Char(string="ID From")
-    military_status = fields.Selection(string="Military Status",
-                                       selection=[('not applicable', 'Not Applicable'), ('postponed', 'Postponed'),
-                                                  ('exempted', 'Exempted'), ('completed', 'Completed'),
-                                                  ('current', 'Currently serving ')])
-    military_number = fields.Char(string="Military Number")
-    religion = fields.Selection(string="Religion",
-                                selection=[('muslim', 'Muslim'), ('christian', 'Christian'), ('other', 'Others')])
-    social_ins_exist = fields.Boolean("Has Social Insurance")
-    social_ins_no = fields.Integer(string="Soical Ins No.")
-    social_ins_title = fields.Char(string="Social Job Title")
-    medical_ins_exist = fields.Boolean("Has Medical Insurance")
-    medical_company = fields.Selection(string="Medical Co.", selection=[('inaya', 'Inaya')])
-    medical_number = fields.Integer(string="Medical No.")
-    mi_date = fields.Date(string="Medical Insurance Date", help='Medical  Insurance Date')
-    section_id = fields.Many2one('hr.department', string="Section", domain=[('department_type', '=', 'section')])
-    full_employee_name = fields.Char(string="Full Name", translate=True)
-    attendance_type = fields.Selection([('in_door', 'IN Door'), ('out_door', 'Out Door')], string='Attendance type')
-    in_direct_parent_id = fields.Many2one('hr.employee', 'Indirect Manager')
-    age = fields.Integer(string="Age",compute="_calculate_age",store=True)#compute="_calculate_age",
-    start_date = fields.Date(string="Start Working At")
-    edu_phase = fields.Many2one(comodel_name="hr.eg.education", string="Education")
-    edu_school = fields.Many2one(comodel_name="hr.eg.school", string="School/University/Institute")
-    edu_major = fields.Char(string="major")
-    edu_grad = fields.Selection([('ex', 'Excellent'), ('vgod', 'Very Good'), ('god', 'Good'), ('pas', 'Pass')],
-                                string="Grad")
-    grad_year = fields.Date(string="Grad. Year")
-    edu_note = fields.Text("Education Notes")
-    experience_y = fields.Integer(compute="_calculate_experience", string="Experience",
-                                  help="experience in our company", store=True)
-    experience_m = fields.Integer(compute="_calculate_experience", string="Experience monthes", store=True)
-    experience_d = fields.Integer(compute="_calculate_experience", string="Experience dayes", store=True)
-    payrolled_employee = fields.Boolean("Payrolled Employee", track_visibility='onchange')
-    employee_arabic_name = fields.Char(string="Arabic Name")
-    private_num = fields.Char(string="Private Number ", required=False, )
-
-    # bank_account_num = fields.Integer(string="Bank Account Number",)
-
     @api.constrains('in_direct_parent_id')
     def _check_parent_id(self):
         for employee in self:
@@ -110,7 +71,7 @@ class employee_fields(models.Model):
                 dob = datetime.strptime(str(emp.birthday), "%Y-%m-%d").date()
                 emp.age = int(int((date.today() - dob).days) / 365)
             else:
-                emp.age=0
+                emp.age = 0
 
     #
     @api.depends("start_date")
@@ -122,27 +83,22 @@ class employee_fields(models.Model):
                 d1 = datetime.strptime(str(emp.start_date), date_format).date()
                 d2 = datetime.strptime(current_date, date_format).date()
                 # r = relativedelta(d2, d1)
-                r = relativedelta.relativedelta(d2 ,d1)
+                r = relativedelta.relativedelta(d2, d1)
 
                 emp.experience_y = r.years
                 emp.experience_m = r.months
                 emp.experience_d = r.days
-    #
-    #
-    # @api.model
+
     def _cron_employee_age(self):
-        print ('22222222222222222222222222222222222')
-        employee_obj=self.env['hr.employee'].search([])
+        employee_obj = self.env['hr.employee'].search([])
         for rec in employee_obj:
             if rec.birthday:
                 dob = datetime.strptime(str(rec.birthday), "%Y-%m-%d").date()
                 rec.age = int(int((date.today() - dob).days) / 365)
-                print ('+++++++++++++++++++',rec.age)
+                print('+++++++++++++++++++', rec.age)
             else:
-                rec.age=0
+                rec.age = 0
 
-
-    # @api.model
     def _cron_employee_exp(self):
         for emp in self.search([]):
             if emp.start_date:
@@ -150,17 +106,16 @@ class employee_fields(models.Model):
                 current_date = (datetime.today()).strftime(date_format)
                 d1 = datetime.strptime(str(emp.start_date), date_format).date()
                 d2 = datetime.strptime(current_date, date_format).date()
-                # r = relativedelta(d2, d1)
-                r = relativedelta.relativedelta(d2 ,d1)
+                r = relativedelta.relativedelta(d2, d1)
 
                 emp.experience_y = r.years
                 emp.experience_m = r.months
                 emp.experience_d = r.days
-            print ('-----------------------------------------------------',emp.experience_y,emp.experience_m,emp.experience_d)
 
 
 class dhn_hr__eg_education(models.Model):
     _name = "hr.eg.education"
+    _description = "Education"
 
     name = fields.Char(string="Education", translate=True)
     note = fields.Char(string="Note", required=False, )
@@ -168,8 +123,7 @@ class dhn_hr__eg_education(models.Model):
 
 class hr_eg_school(models.Model):
     _name = "hr.eg.school"
+    _description = "Education School"
 
     name = fields.Char(string="School name", translate=True)
     note = fields.Char(string="Note", required=False, )
-
-
