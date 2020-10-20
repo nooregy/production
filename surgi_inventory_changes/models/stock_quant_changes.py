fffc5e8554ca313b88745486436bd7b97201bc03
surgi-tech <59487271+surgi-tech@users.noreply.github.com>
2020-10-20 07:40:05 +0200

    product_categ_id

--- surgi_inventory_changes/models/stock_quant_changes.py
+++ surgi_inventory_changes/models/stock_quant_changes.py
@@ -8,4 +8,4 @@ class stock_quant_inherit_wizard(models.Model):
     #u_p_usd = fields.Float(string="U. Price in USD")
 
     product_group = fields.Char(srting="Group", related="product_id.product_group", store=True)
-    product_categ_id = fields.Char(srting="Product Category", related="product_id.categ_id", store=True)
+    product_categ_id = fields.Many2one('product.category', related='product_id.categ_id', store=True)
